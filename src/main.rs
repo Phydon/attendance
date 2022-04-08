a31ef9d94dd3b617596051ea7766619cd9798d47
Phydon <leann.phydon@gmail.com>
2022-04-08 21:22:23 +0200

    user input -> into table

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,57 @@
 use prettytable::{Table, Row, Attr, Cell, color, format};
 use std::{io, fs};
 use std::io::Write;
-// use std::collections::BTreeMap;
+use std::collections::BTreeMap;
 use chrono::Local;
 
 const FILEPATH: &str = "/home/phydon/main/attendance/logfile.txt";
 
 fn main() {
-    println!("Input: ");
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => println!("Your input was: {}", input),
-        Err(error) => eprintln!("Failed to read input. {}", error),
-    }
-
-    // let mut container: BTreeMap<_,_> = BTreeMap::new();
     let datetime = Local::now().to_string();
 
+    let mut container: BTreeMap<_,_> = BTreeMap::new();
+    let keys = vec!["LOG", "PF", "TEF"];
+    
+    println!("Enter \"Y\" / \"y\" for YES or \"N\" / \"n\" for NO");
+
+    for key in keys {
+        println!("{} attendant?", key);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        container.insert(key, input.to_uppercase());
+    }
+
+    let mut idx: i32 = 0;
     let mut table = Table::new();
     table.set_format(*format::consts::FORMAT_BOX_CHARS);
     table.set_titles(Row::new(vec![
             Cell::new(&datetime)
                 .with_style(Attr::ForegroundColor(color::RED))
                 .with_hspan(3)]));
-    table.add_row(row![FdBwbc => "Index", "Attendance", "Yes / No"]);
-    table.add_row(row!["1", Fb->"PF", c->"[ ]"]);
-    table.add_row(row!["2", Fb->"LOG", c->"[ ]"]);
+    table.add_row(row![FdBwbl->"Index", FdBwbc->"Attendance", FdBwbc->"Yes / No"]);
+    for (key, value) in &container {
+        table.add_row(row![idx, Fb->key, c->value]);
+        idx += 1;
+    }
 
     write_to_file(&mut table).expect("Failed to write to file");
 
     table.printstd();
+
+    loop {
+        println!("Done?");
+        println!("Press \"q\" to quit!");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {},
+            Err(error) => eprintln!("Failed to read input. {}", error),
+        }
+
+        if input.contains(&String::from("q"))  {
+            std::process::exit(0);
+        }
+    }
+
 }
 
 fn write_to_file(content: &mut Table) -> io::Result<()> {
