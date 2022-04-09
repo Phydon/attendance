7829b8e04da9135019c9562b94861276103f6cc4
Phydon <leann.phydon@gmail.com>
2022-04-09 12:24:49 +0200

    extracted into functions

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,19 @@
 #[macro_use] extern crate prettytable;
 use prettytable::{Table, Row, Attr, Cell, color, format};
+use chrono::Local;
+
 use std::{io, fs};
 use std::io::Write;
 use std::collections::BTreeMap;
-use chrono::Local;
 
 const FILEPATH: &str = "/home/phydon/main/attendance/logfile.txt";
 
 fn main() {
     let datetime = Local::now().to_string();
+    let container = check_attendance();
+    let table = create_table(datetime, container);
 
-    let mut container: BTreeMap<_,_> = BTreeMap::new();
-    let keys = vec!["LOG", "PF", "TEF"];
-    
-    println!("Enter \"Y\" / \"y\" for YES or \"N\" / \"n\" for NO\n");
-
-    for key in keys {
-        loop {
-            println!("{} attendant?", key);
-
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Failed to read input");
-            let input = input.strip_suffix("\n").unwrap().to_uppercase();
-
-            if input.len() == 1 && input.contains(&String::from("Y")) {
-                container.insert(key, "Y".to_string());
-                break;
-            } else if input.len() == 1 && input.contains(&String::from("N")) {
-                container.insert(key, "N".to_string());
-                break;
-            } else {
-                println!("Not valid: {}", input);
-                println!("Enter \"Y\" / \"y\" for YES or \"N\" / \"n\" for NO\n");
-            }
-        }
-    }
-
-    let mut idx: i32 = 0;
-    let mut table = Table::new();
-
-    table.set_format(*format::consts::FORMAT_BOX_CHARS);
-    table.set_titles(Row::new(vec![
-            Cell::new(&datetime)
-                .with_style(Attr::ForegroundColor(color::RED))
-                .with_hspan(3)]));
-    table.add_row(row![FdBwbl->"Index", FdBwbc->"Attendance", FdBwbc->"Yes / No"]);
-
-    for (key, value) in &container {
-        table.add_row(row![idx, Fb->key, c->value]);
-        idx += 1;
-    }
-
-    write_to_file(&mut table).expect("Failed to write to file");
+    write_to_file(&table).expect("Failed to write to file");
 
     table.printstd();
 
@@ -71,10 +33,9 @@ fn main() {
             std::process::exit(0);
         }
     }
-
 }
 
-fn write_to_file(content: &mut Table) -> io::Result<()> {
+fn write_to_file(content: &Table) -> io::Result<()> {
     let mut file = fs::OpenOptions::new()
         .append(true)
         .create(true)
@@ -83,3 +44,52 @@ fn write_to_file(content: &mut Table) -> io::Result<()> {
     writeln!(file, "{}", content)?;
     Ok(())
     }
+
+fn check_attendance() -> BTreeMap<String, String> {
+    let mut container: BTreeMap<_,_> = BTreeMap::new();
+    let keys: Vec<String> = vec!["LOG".to_string(), "PF".to_string(), "TEF".to_string()];
+    
+    println!("Enter \"Y\" for YES or \"N\" for NO\n");
+
+    for key in keys {
+        loop {
+            println!("{} attendant?", key);
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read input");
+            let input = input.strip_suffix("\n").unwrap().to_uppercase();
+
+            if input.len() == 1 && input.contains(&String::from("Y")) {
+                container.insert(key, "Y".to_string());
+                break;
+            } else if input.len() == 1 && input.contains(&String::from("N")) {
+                container.insert(key, "N".to_string());
+                break;
+            } else {
+                println!("Not valid: {}", input);
+                println!("Enter \"Y\" for YES or \"N\" for NO\n");
+            }
+        }
+    }
+
+    container
+}
+
+fn create_table(datetime: String , container: BTreeMap<String, String>) -> Table {
+    let mut idx: i32 = 0;
+    let mut table = Table::new();
+
+    table.set_format(*format::consts::FORMAT_BOX_CHARS);
+    table.set_titles(Row::new(vec![
+            Cell::new(&datetime)
+                .with_style(Attr::ForegroundColor(color::RED))
+                .with_hspan(3)]));
+    table.add_row(row![FdBwbl->"Index", FdBwbc->"Attendance", FdBwbc->"Yes / No"]);
+
+    for (key, value) in &container {
+        table.add_row(row![idx, Fb->key, c->value]);
+        idx += 1;
+    }
+
+    table
+}
