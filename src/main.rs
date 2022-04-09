de41ca0c27331890c5b00ea5d4b58bf3ae0cb9f2
Phydon <leann.phydon@gmail.com>
2022-04-09 20:38:53 +0200

    clear screen added

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 use prettytable::{Table, Row, Attr, Cell, color, format};
 use chrono::Local;
 
-use std::{io, fs};
+use std::process::Command;
+use std::time::Duration;
+use std::{io, fs, thread};
 use std::io::Write;
 use std::collections::BTreeMap;
 
@@ -15,59 +17,48 @@ fn main() {
 
     write_to_file(&table).expect("Failed to write to file");
 
+    exec_clear().expect("Failed to clear screen");
     table.printstd();
 
-    loop {
-        println!("Done?");
-        println!("Press \"q\" to quit!");
-
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {},
-            Err(error) => eprintln!("Failed to read input. {}", error),
-        }
-        
-        let input = input.strip_suffix("\n").unwrap();
-
-        if input.len() == 1 && input.contains(&String::from("q"))  {
-            std::process::exit(0);
-        }
-    }
+    are_u_done();
 }
 
-fn write_to_file(content: &Table) -> io::Result<()> {
-    let mut file = fs::OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(FILEPATH)?;
-
-    writeln!(file, "{}", content)?;
-    Ok(())
+fn exec_clear() -> io::Result<()> {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd").args(["/C", "clear"]).status()?;
+        Ok(())
+    } else {
+        Command::new("clear").status()?;
+        Ok(())
     }
+}
 
 fn check_attendance() -> BTreeMap<String, String> {
     let mut container: BTreeMap<_,_> = BTreeMap::new();
     let keys: Vec<String> = vec!["LOG".to_string(), "PF".to_string(), "TEF".to_string()];
     
-    println!("Enter \"Y\" for YES or \"N\" for NO\n");
-
     for key in keys {
         loop {
+            exec_clear().expect("Failed to clear screen");
+            println!("Enter \"Y\" for YES or \"N\" for NO\n");
             println!("{} attendant?", key);
 
             let mut input = String::new();
             io::stdin().read_line(&mut input).expect("Failed to read input");
             let input = input.strip_suffix("\n").unwrap().to_uppercase();
 
-            if input.len() == 1 && input.contains(&String::from("Y")) {
-                container.insert(key, "Y".to_string());
+            let yes: String = "Y".to_string();
+            let no: String = "N".to_string();
+
+            if input.eq(&yes) {
+                container.insert(key, yes);
                 break;
-            } else if input.len() == 1 && input.contains(&String::from("N")) {
-                container.insert(key, "N".to_string());
+            } else if input.eq(&no) {
+                container.insert(key, no);
                 break;
             } else {
                 println!("Not valid: {}", input);
-                println!("Enter \"Y\" for YES or \"N\" for NO\n");
+                thread::sleep(Duration::from_millis(1500));
             }
         }
     }
@@ -92,4 +83,33 @@ fn create_table(datetime: String , container: BTreeMap<String, String>) -> Table
     }
 
     table
+}
+
+fn write_to_file(content: &Table) -> io::Result<()> {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(FILEPATH)?;
+
+    writeln!(file, "{}", content)?;
+    Ok(())
+    }
+
+fn are_u_done() {
+    loop {
+        println!("Done?");
+        println!("Press \"q\" to quit!");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+
+        exec_clear().expect("Failed to clear screen");
+        
+        let input = input.strip_suffix("\n").unwrap();
+        let quit: String = "q".to_string();
+
+        if input.eq(&quit)  {
+            std::process::exit(0);
+        }
+    }
 }
